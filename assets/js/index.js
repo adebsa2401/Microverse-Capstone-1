8ac4ba0b653d7e131fed8172c461ad55c3948a76
Ben Sales <adebsa2401@gmail.com>
2022-10-20 08:18:40 +0100

    feat: load dynamically the speakers list

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,65 @@
+// speakers data array
+const SPEAKERS = [
+  {
+    photo: 'speaker_01.png',
+    name: 'Yochai Benkler',
+    role: 'Berkman Professor of Entrepreunarial Legal Studies at Havard Law School',
+    description: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
+  },
+  {
+    photo: 'speaker_01.png',
+    name: 'Yochai Benkler',
+    role: 'Berkman Professor of Entrepreunarial Legal Studies at Havard Law School',
+    description: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
+  },
+  {
+    photo: 'speaker_01.png',
+    name: 'Yochai Benkler',
+    role: 'Berkman Professor of Entrepreunarial Legal Studies at Havard Law School',
+    description: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
+  },
+  {
+    photo: 'speaker_01.png',
+    name: 'Yochai Benkler',
+    role: 'Berkman Professor of Entrepreunarial Legal Studies at Havard Law School',
+    description: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
+  },
+  {
+    photo: 'speaker_01.png',
+    name: 'Yochai Benkler',
+    role: 'Berkman Professor of Entrepreunarial Legal Studies at Havard Law School',
+    description: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
+  },
+  {
+    photo: 'speaker_01.png',
+    name: 'Yochai Benkler',
+    role: 'Berkman Professor of Entrepreunarial Legal Studies at Havard Law School',
+    description: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
+  },
+];
+
+function getSpeakerCard(speaker) {
+  const card = document.createElement('div');
+
+  card.innerHTML = `
+    <div class="speaker-card">
+      <img src="assets/images/speakers/${speaker.photo}" alt="speaker-img"/>
+      <div class="speaker-info">
+        <h3 class="speaker-name">${speaker.name}</h3>
+        <p class="speaker-role">
+          ${speaker.role}
+        </p>
+        <hr/>
+        <p class="speaker-description">
+          ${speaker.description}
+        </p>
+      </div>
+    </div>
+  `.trim();
+
+  return card.firstChild;
+}
+
 // toggle More/Less button to display more/less speakers
 document.getElementById('more').addEventListener('click', (event) => {
   event.target.querySelector('svg').classList.toggle('fa-chevron-down');
@@ -9,4 +71,9 @@ document.getElementById('more').addEventListener('click', (event) => {
   Array.from(document.querySelectorAll('.speaker-card:nth-child(n+3)')).forEach((element) => {
     element.style.display = displayMore ? 'none' : 'flex';
   });
+});
+
+// load dynamically the speakers list
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('speakers-list').append(...SPEAKERS.map(getSpeakerCard));
 });
